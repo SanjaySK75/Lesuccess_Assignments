ae5341460b9f855057c1c6d57cf9de99d51aa6c3
Sanjay Kumar <sksanjaymdu7515@gmail.com>
2025-05-27 20:45:45 +0530

    Add files via upload

--- Assignment/Assignment_3/ArrayRotation.java
+++ Assignment/Assignment_3/ArrayRotation.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ArrayRotation {
     public static void main(String[] args){
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the array size");
         int n = sc.nextInt();
         System.out.println("Enter the Array elements");
@@ -12,5 +12,26 @@ public class ArrayRotation {
         }
         System.out.println("Enter the k Value : ");
         int k = sc.nextInt();
+        rotate(arr, k);
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]+ " ");
+        }
+    }
+    static void rotate(int[] arr,int k){
+        reverse(arr , 0 , k-1);
+        reverse(arr , k , arr.length-1);
+        reverse(arr , 0 , arr.length-1);
+    }
+    static void reverse(int[] arr,int l,int r){
+        while(l<r){
+            swap(arr,l,r);
+            l++;
+            r--;
+        }
+    }
+    static void swap(int[] arr,int l,int r){
+        int temp = arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
     }
 }
